954bac18dff4b3f1972f4f0cff5aa2a79b4cfff8
Dilshod-22 <myprofileforgithub@gmail.com>
2023-11-09 10:10:17 +0500

    update

--- config/passport.js
+++ config/passport.js
@@ -19,7 +19,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8001/api/auth/google/callback",
+    callbackURL: "https://bekorchili.onrender.com/api/auth/google/callback",
     profileFields: ['emails'] 
   },
   async function(accessToken, refreshToken, profile, cb) {
